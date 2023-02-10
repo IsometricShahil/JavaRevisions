76a1aa34c40f0fa10573f4b4ae67300d68707963
Shahil Ahmed <53872662+IsometricShahil@users.noreply.github.com>
2023-02-10 16:52:48 +0530

    Primitives revision added

--- src/Primitives.java
+++ src/Primitives.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Primitives {
+public class Primitives {
+    public static void main(String[] args) {
+        byte b = 23;
+        short s = 46;
+        int i = 78;
+        long l = 84845888420487L; // Notice the L
+        float f = 8.37524f; // Notice the f
+        double d = 5.94756382048; // 5.94756382048d is valid but not needed
+        char c = 'C'; // Single quotes are mandatory for chars
+        boolean bool = true; // or false
+
+        System.out.println("Byte max: " + Byte.MAX_VALUE);
+        System.out.println("Short max: " + Short.MAX_VALUE);
+        System.out.println("Integer max: " + Integer.MAX_VALUE);
+        System.out.println("Long max: " + Long.MAX_VALUE);
+        System.out.println("Float max: " + Float.MAX_VALUE);
+        System.out.println("Double max: " + Double.MAX_VALUE);
+    }
 }
